9b59a0c7c5e45d77fb3b3df667e273ae4dac35c6
kayhide <kayhide@gmail.com>
2016-05-21 16:57:18 +0900

    add test for #even?.

--- spec/numerics_spec.rb
+++ spec/numerics_spec.rb
@@ -1,15 +1,31 @@
 require_relative 'spec_helper'
 
 describe Numerics do
-  it '3' do
-    assert {
-      Numerics.odd?(3) == true
-    }
+  describe '#odd?' do
+    it '3' do
+      assert {
+        Numerics.odd?(3) == true
+      }
+    end
+
+    it '4' do
+      assert {
+        Numerics.odd?(4) == false
+      }
+    end
   end
 
-  it '4' do
-    assert {
-      Numerics.odd?(4) == false
-    }
+  describe '#even?' do
+    it '3' do
+      assert {
+        Numerics.even?(3) == false
+      }
+    end
+
+    it '4' do
+      assert {
+        Numerics.even?(4) == true
+      }
+    end
   end
 end
